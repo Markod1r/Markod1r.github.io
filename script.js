8804b791f0c9c5457fc8b8b46a55839829faaf3e
marcoismakalew <marcoismakalew@gmail.com>
2025-03-31 12:22:52 +0800

    create object for section deskripsi

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let i = 1;
+let person = {
+	name: "Marcois",
+	hello: "Hello, I'm",
+	who: "I'm Human",
+};
+
+document.getElementById("name").innerHTML = person.name;
+document.getElementById("hello").innerHTML = person.hello;
+document.getElementById("who").innerHTML = person.who;
