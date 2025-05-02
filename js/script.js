c87c8b6b3b45b82963773915e68ef1d5bafe8985
marcoismakalew <marcoismakalew@gmail.com>
2025-05-02 15:15:20 +0800

    add: animation on card

--- js/script.js
+++ js/script.js
@@ -10,11 +10,16 @@ class human {
 	}
 }
 
-function tema() {
-	return;
-}
-
 let marco = new human("marco");
-
 document.getElementById("name").innerHTML = marco.sayHello();
-document.getElementById("tema").addEventListener("click", tema);
+
+let card = document.querySelectorAll(".card");
+
+card.forEach((card) => {
+	card.addEventListener("mouseover", () => {
+		card.style.transform = "scale(1.2)";
+	});
+	card.addEventListener("mouseout", () => {
+		card.style.transform = "scale(1)";
+	});
+});
